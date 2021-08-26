46ee84887071d2d1517bff3705b574b78667e52e
Gregor Christie <gregor552@icloud.com>
2021-08-26 23:39:01 +0100

    inital yfinance implementation + readme

---
+++ readme.md
@@ -0,0 +1,8 @@
+# stockCalc
+
+For when you wonder how much money you would have if you had invested it...
+
+## Requirements
+
+* [Python3](https://www.python.org)
+* [yfinance](https://github.com/ranaroussi/yfinance)
--- stockCalc.py
+++ stockCalc.py
@@ -1,8 +1,32 @@
 # Stock Calc
 # By Gregor Christie
 
-initalStockValue = float(input("Enter intial share price: "))
-depositValue = float(input("Enter buy amount in stocks currency: "))
-finalStockValue = float(input("Enter the final share price: "))
+import locale
+import yfinance as yf
 
-print((depositValue/initalStockValue)*finalStockValue)
+def main():
+
+    tickerName = input("Enter stock symbol: ")
+
+    ticker = yf.Ticker(tickerName)
+
+    tickerInfo = ticker.info
+
+    if(tickerInfo["financialCurrency"]=="USD"):
+        locale.setlocale(locale.LC_ALL, 'en_US')
+    elif(tickerInfo["financialCurrency"]=="GBP"):
+        locale.setlocale(locale.LC_ALL, 'en_GB')
+    else:
+        locale.setlocale(locale.LC_ALL, 'en_US')
+
+    initalStockValue = float(input("Enter intial share price: "))
+    depositValue = float(input("Enter buy amount in stocks currency: "))
+    # finalStockValue = float(input("Enter the final share price: "))
+    finalStockValue = ticker.info["currentPrice"]
+    print("The current stock value is %s" % (locale.currency(finalStockValue)))
+    
+    finalBalance = (depositValue/initalStockValue)*finalStockValue
+    print(locale.currency(finalBalance))
+
+if __name__ == "__main__":
+    main()
